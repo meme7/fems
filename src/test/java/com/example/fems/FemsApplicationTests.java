685eb7e1713478c0b5afebd26555659f4223e9b2
meme7 <manlopez77@gmail.com>
2022-05-09 20:34:05 -0600

    setup

---
+++ src/test/java/com/example/fems/FemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
